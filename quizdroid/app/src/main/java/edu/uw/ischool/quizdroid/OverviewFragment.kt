d09e28c1716c53e1e6b964690e08f8fd78ec8c25
charlyecastro <castroc@uw.edu>
2018-04-26 13:50:46 -0700

    overview file

---
+++ quizdroid/app/src/main/java/edu/uw/ischool/quizdroid/OverviewFragment.kt
@@ -0,0 +1,19 @@
+package edu.uw.ischool.quizdroid
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class OverviewFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
