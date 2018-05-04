cd8158541170bf07f71343f7b52f35fb9c8680b9
charlyecastro <castroc@uw.edu>
2018-05-04 13:25:46 -0700

    updated read me

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # quizdroid
+This is an app that quizzes the user on trivial topics
 
-Practiced with [intent](https://developer.android.com/reference/android/content/Intent.html) to build a quiz app.
+### Used the following to build this app:
+
+#### Android
+- [intent](https://developer.android.com/reference/android/content/Intent.html)
+- [Fragments]( https://developer.android.com/guide/components/fragments)
+- [BufferedInputStream](https://developer.android.com/reference/java/io/BufferedInputStream)
+- [AsyncTask](https://developer.android.com/reference/android/os/AsyncTask)
+
+
+#### JSON
+* I made my own custom  [JSON](https://charlyecastro.github.io/myJSON/myQuestions.json) file that has my own unique quiz 
+* Also used The supplied [JSON](http://tednewardsandbox.site44.com/questions.json) file
+
+#### Libraries
+* Used [Gson](https://google.github.io/gson/apidocs/com/google/gson/Gson.html) to make objects from the parsed JSON
--- quizdroid/app/src/main/java/edu/uw/ischool/quizdroid/QuizApp.kt
+++ quizdroid/app/src/main/java/edu/uw/ischool/quizdroid/QuizApp.kt
@@ -42,14 +42,16 @@ class QuizApp : Application() {
 
        // val top = ArrayList<Topic>()
         var url = ""
+        var url2 = ""
 
         constructor() {
+            url2 = "https://charlyecastro.github.io/myJSON/myQuestions.json"
             url = "http://tednewardsandbox.site44.com/questions.json"
             execute(url).get()
         }
 
         override fun doInBackground(vararg p0: String?): String {
-            val connection = URL(url).openConnection() as HttpURLConnection
+            val connection = URL(url2).openConnection() as HttpURLConnection
 
             var input = ""
 
@@ -61,13 +63,9 @@ class QuizApp : Application() {
             }
 
             val obj = JSONArray(input)
-//            Log.i("test", obj.toString())
-//            Log.i("input",input)
 
             val gson = GsonBuilder().create()
             topics = gson.fromJson(input, Array<Topic>::class.java)
-//            println(topics[0].title)
-//            println(topics[0].desc)
             return input
         }
 
@@ -84,34 +82,7 @@ class QuizApp : Application() {
             return list
         }
 
-
-
         //        init {
-//            val url = "http://tednewardsandbox.site44.com/questions.json"
-//            val request = Request.Builder().url(url).build()
-//
-//            val client = OkHttpClient()
-//            client.newCall(request).enqueue(object: Callback {
-//
-//                override fun onResponse(call: Call?, response: Response?) {
-//                    val body = response?.body()?.string()
-//                    //println(body)
-//
-//                    val gson = GsonBuilder().create()
-//                    topics = gson.fromJson(body, Array<Topic>::class.java)
-//                }
-//
-//                override fun onFailure(call: Call?, e: IOException?) {
-//                    Log.e("ERROR", "failed to execute request")
-//                }
-//
-//            } )
-//        }
-
-//        fun fetchJson() {
-//
-//            var top : Array<Topic>
-//
 //            val url = "http://tednewardsandbox.site44.com/questions.json"
 //            val request = Request.Builder().url(url).build()
 //
@@ -135,70 +106,3 @@ class QuizApp : Application() {
     }
 
 }
-
-
-
-//        val topicList = arrayOf("Math", "Physics","Marvel Super Heroes", "Movies", "Soccer", "Random")
-//        val topicDesc = arrayOf("A lot of numbers", "Something that won't let you defy it's laws", "The better version of DC Super Heroes (Except for Batman)",
-//                "It's like a book, but it can talk and move", "The real football", "Shit. I dont know")
-//
-//
-//        // Subject Questions
-//        val mathQ = arrayOf("1 + 1 equals?", "Which is the formula for the Pythagorean theorem", "Which of the following is not a math subject")
-//        val physicsQ = arrayOf("E equals?", "How many laws does Newton have?", "What is Earths gravitational acceleration?")
-//        val marvelQ = arrayOf("Who is Spider Man?", "Who is NOT a member of the Avengers?", "Who owns Marvel?")
-//        val moviesQ = arrayOf("Which of these movies was released in 2008?", "Which movie won the most Oscars in 1997", "The Disaster Artist is based on which film?")
-//        val soccerQ = arrayOf("How many Balon d'Or has Cristiano Ronaldo won?", "Who is currently the top lead scorer in the English Premiere League?", "Where is the 2018 World Cup taking place?")
-//        val randomQ = arrayOf("Which of these companies own ESPN?", "What is the most popular show on Netflix?", "Do you matter?")
-//
-//        val questions = arrayOf(mathQ, physicsQ, marvelQ, moviesQ, soccerQ, randomQ)
-//
-//
-//        //Subject Options
-//        val mathO = arrayOf(arrayOf("11", "2", "-100", "81"),
-//                arrayOf("b - (b * a) = c", "PI * R^2 = A", "a^2 + b^2 = c^2", "MC^2 = E"),
-//                arrayOf("Physical Education", "Algebra", "Geometry", "Calculus"))
-//
-//        val physicsO = arrayOf(arrayOf("mc^2", "d/t", "l * w", "mv"),
-//                arrayOf("4", "8", "3", "15"),
-//                arrayOf("3.14 m/s^2", "9.807 m/s^2", "5.34 m/s^2", "9.801 m/s^2"))
-//
-//        val marvelO = arrayOf(arrayOf("Tony Stark", "Peter Parker", "Bruce Wayne", "Bruce Banner"),
-//                arrayOf("Hulk", "Black Widow", "Bruce Banner", "Wolverine"),
-//                arrayOf("Walt Disney", "COMCAST", "FOX", "Time Warner"))
-//
-//        val moviesO = arrayOf(arrayOf("Dead Pool", "Forrest Gump", "The Dark Knight", "Zodiac"),
-//                arrayOf("Gattaca", "Forrest Gump", "Good Will Hunting", "Titanic"),
-//                arrayOf("The Usual Suspects", "The Room", "Forrest Gump", "Her"))
-//
-//        val soccerO = arrayOf(arrayOf("5", "3", "2", "7"),
-//                arrayOf("Harry Kane", "Sergio Aguero", "Andres Iniesta", "Mo Salah"),
-//                arrayOf("Russia", "Germany", "Argentina", "Paraguay"))
-//
-//        val randomO = arrayOf(arrayOf("Nike", "Walt Disney", "Time Warner", "Adidas"),
-//                arrayOf("Stranger Things", "On My Block", "The Office", "Rick and Morty"),
-//                arrayOf("I Don't Know", "Nope", "Yes", "Who Cares"))
-//
-//        val options = arrayListOf(mathO, physicsO, marvelO, moviesO, soccerO, randomO)
-//
-//
-//        val mathA = arrayOf(1, 2, 0)
-//        val physicsA = arrayOf(0, 2, 1)
-//        val marvelA = arrayOf(1, 3, 0)
-//        val moviesA = arrayOf(2, 3, 1)
-//        val soccerA = arrayOf(0, 3, 1)
-//        val randomA = arrayOf(1, 2, 2)
-//
-//        val answers = arrayOf(mathA, physicsA, marvelA, moviesA, soccerA, randomA)
-
-//lateinit var topics : List<Topic>
-
-//        init {
-//            for (i in topicList.indices) {
-//                var quizzes = ArrayList<Quiz>()
-//                for(j in questions[i].indices) {
-//                    quizzes.add(Quiz(questions[i][j], answers[i][j], options[i][j]))
-//                }
-//                topics.add(Topic(topicList[i], topicDesc[i], quizzes))
-//            }
-//        }
